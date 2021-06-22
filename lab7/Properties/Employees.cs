a8dd951aae044cb3b63874cc86722d83368a48d3
haduynhat2002 <nhathdth2009032@gmail.com>
2021-06-22 23:52:13 +0700

    bai tap lab5

---
+++ lab7/Properties/Employees.cs
@@ -0,0 +1,14 @@
+﻿namespace lab7.Properties
+{
+    public class Employees
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+        
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}, Salary: {Salary}";
+        }
+    }
+}
